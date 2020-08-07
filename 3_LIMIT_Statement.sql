eeadbf43b105a46654b5fcdacfd6f1910b92a294
Pratik Patel <ppratikbp@gmail.com>
2020-08-07 17:01:49 -0400

    Update 3_LIMIT_Statement.sql

--- 3_LIMIT_Statement.sql
+++ 3_LIMIT_Statement.sql
@@ -1,6 +1,4 @@
 /* Using LIMIT to limit the number of rows returned in a query */
-SELECT occurred_at, 
-		account_id,
-        channel
+SELECT occurred_at, account_id,channel
    FROM web_events
-    LIMIT 15; /*  We are limiting to the first 15 rows  */
+   LIMIT 15; /*  We are limiting to the first 15 rows  */
