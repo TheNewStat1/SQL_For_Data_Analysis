3dfaa588debb7aa9f13469854240fab3a75006b6
Pratik Patel <pratikp@thenewstatistician.com>
2020-08-24 16:49:43 -0400

    update repo

--- 33_POSITION_POSTR_and SUBSTR Functions.sql
+++ 33_POSITION_POSTR_and SUBSTR Functions.sql
@@ -1,4 +1,4 @@
-/* Using the POISITION and POSTR functions */
+/* Using the POISITION and STRPOS functions */
 
 SELECT LEFT(primary_poc, STRPOS(primary_poc, ' ') -1 ) first_name, 
 RIGHT(primary_poc, LENGTH(primary_poc) - STRPOS(primary_poc, ' ')) last_name
@@ -7,3 +7,4 @@ FROM accounts;
 SELECT LEFT(name, STRPOS(name, ' ') -1 ) first_name, 
        RIGHT(name, LENGTH(name) - STRPOS(name, ' ')) last_name
 FROM sales_reps;
+
