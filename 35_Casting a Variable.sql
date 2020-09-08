eaf963f8dc0122ed23b8823d9187f39b6e1941f1
Pratik Patel <ppratikbp@gmail.com>
2020-09-07 20:33:27 -0400

    update repository

---
+++ 35_Casting a Variable.sql
@@ -0,0 +1,11 @@
+/* This is how to cast a variable into a different type in SQL */
+
+SELECT *
+FROM sf_crime_data
+LIMIT 10;
+
+SELECT date orig_date, (SUBSTR(date, 7, 4) || '-' || LEFT(date, 2) || '-' || SUBSTR(date, 4, 2)) new_date
+FROM sf_crime_data;
+
+SELECT date orig_date, (SUBSTR(date, 7, 4) || '-' || LEFT(date, 2) || '-' || SUBSTR(date, 4, 2))::DATE new_date
+FROM sf_crime_data;
